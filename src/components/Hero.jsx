5b48e3c4bc5cb3bb27c6ec3250ec97e1372295b2
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-07-14 00:31:06 +0530

    add all

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@
 import React from "react";
 import { FlipWords } from "./ui/flip-words";
 import { Roboto } from "next/font/google";
-import { FaLinkedin ,  FaInstagramSquare } from "react-icons/fa";
-import { Button } from "@/components/ui/button"
+import { FaLinkedin, FaInstagramSquare } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Skillhero from "./Skillhero";
-
+import Image from "next/image";
 import { SlArrowDown } from "react-icons/sl";
 
 const roboto = Roboto({
@@ -20,20 +20,18 @@ export default function Hero() {
   return (
     <main className="p-8">
       <div className="grid gap-5 mt-16 sm:grid-cols-12">
-       
-        <div className=" min-h-[15rem] mt-6 p-3 sm:col-span-6">
+        <div className="min-h-[15rem] mt-6 p-3 sm:col-span-6">
           <div className="flex flex-col justify-center">
-          <h1 className="text-3xl p-2">Hello i.m </h1>
+            <h1 className="text-3xl p-2">Hello I&apos;m</h1>
             <h1 className="text-3xl p-2">Parikhit Kurmi</h1>
-            <h1 className="text-3xl p-2 text-cyan-400"> 
-            </h1>
+            <h1 className="text-3xl p-2 text-cyan-400"></h1>
             <div className="text-3xl p-2">
-              Welcome to My Protfolio, Let's <FlipWords words={words} />
+              Welcome to My Portfolio, Let&apos;s <FlipWords words={words} />
             </div>
           </div>
           <div className="flex flex-row p-3 space-x-2">
             <Button variant="ghost" className="bg-gray">
-              <FaInstagramSquare />
+              <FaLinkedin />
             </Button>
             <Button variant="ghost" className="bg-gray">
               <FaInstagramSquare />
@@ -45,33 +43,29 @@ export default function Hero() {
               <FaInstagramSquare />
             </Button>
           </div>
-          
-          <div className=" grid grid-flow-col w-1/2 sm:grid-cols-12 gap-3">
-        
+          <div className="grid grid-flow-col w-1/2 sm:grid-cols-12 gap-3">
             <button className="sm:w-[8rem] shadow-[inset_0_0_0_2px_#19f3f3] px-1 py-2 rounded-lg tracking-widest font-medium bg-transparent hover:bg-[#60c8ee] hover:text-white dark:text-neutral-200 transition duration-200 sm:col-span-6">
-             Play
+              Play
             </button>
-           
-            <button className=" sm:w-[8rem] px-1 py-2 rounded-lg bg-[#19f3f3] text-white font-medium transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-cyan-500 sm:col-span-6">
+            <button className="sm:w-[8rem] px-1 py-2 rounded-lg bg-[#19f3f3] text-white font-medium transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-cyan-500 sm:col-span-6">
               Invert it
             </button>
           </div>
         </div>
-        <div className=" min-h-[5rem] sm:col-span-6">
-        <img src="/mainIconsdark.svg" alt="Main Icon Dark" className=" h-[28.5rem] , w-[38rem]"
-         />
+        <div className="min-h-[5rem] sm:col-span-6">
+          <Image
+            src="/mainIconsdark.svg"
+            alt="Main Icon Dark"
+            className="h-[28.5rem] w-[38rem]"
+            width={380}
+            height={285}
+          />
         </div>
       </div>
       <div className="flex justify-center items-center">
         <SlArrowDown className="w-[2rem] text-white" />
       </div>
-
-      <Skillhero/>
-
-
-
-
-      
+      <Skillhero />
     </main>
   );
 }
